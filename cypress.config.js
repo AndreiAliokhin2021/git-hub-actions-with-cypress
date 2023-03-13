15a8c4f2f4f922acf6ef9a1ec6a90b30bebb760d
Andrei Aliokhin <92366136+AndreiAliokhin2021@users.noreply.github.com>
2023-03-13 17:24:42 +0100

    Create main.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'd0edf90c-6609-4cfe-8e98-910393f29f56',
+  projectId: 'pwugvx',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
