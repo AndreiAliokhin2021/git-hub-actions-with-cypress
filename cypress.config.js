d24936b61db3457c10ce55619ae5e3451f7b9461
andrei.aliokhin <andrei.aliokhin@itechart-group.com>
2023-03-13 15:51:40 +0100

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'd0edf90c-6609-4cfe-8e98-910393f29f56',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
